807d957eb4c3b1b53f73ab2cbd9a7d3e3bed5648
Andrew <andrew@aven.com>
2021-02-22 06:56:17 -0800

    rename modules again

--- check_all.sh
+++ check_all.sh
@@ -3,7 +3,7 @@
 echo "Begin check..." \
 && black . \
 && python gen_pyi.py \
-&& black -l 100 -t py36 --pyi --fast matplotlib_stubs/pyplot.pyi.in \
+&& black -l 100 -t py36 --pyi --fast dsTypesMatplotlib/pyplot.pyi.in \
 && flake8 *-stubs \
 && python -m pytest -vv tests/ \
 && mypy tests \
